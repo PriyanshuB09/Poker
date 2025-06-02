6d0329e5821da7ab52d4db291f3636aee4d50e37
Priyanshu Biswal <priy.biswal@gmail.com>
2025-06-02 01:04:45 -0400

    bug fix

--- index.js
+++ index.js
@@ -404,7 +404,6 @@ io.on('connection', socket => {
   
   socket.on('signup-submit-req', data => {
     console.log('received');
-    console.log(app);
     addEmitter().open('users').add(data).commit().then(() => console.log('sent data'));
   });
 });
