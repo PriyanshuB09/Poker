1a78121630438f52226c6e9e143b7bf89f76210f
Priyanshu Biswal <priy.biswal@gmail.com>
2025-06-02 00:42:24 -0400

    removed analytics

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -18,7 +17,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 /**
