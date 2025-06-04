a2a31e382d6bb60e66f6586019a14278192b7b6a
Ansh2021 <77895114+Ansh2021@users.noreply.github.com>
2025-06-04 19:59:34 -0400

    two modals added (PLEASE BE CAREFUL WITH THIS AS SOME CODE MAY BE MISSING!)

--- public/script.js
+++ public/script.js
@@ -1,98 +1,118 @@
 const socket = io();
 
-let userData = {
-  
-};
+let userData = {};
 
 let referrableID;
 
 $(() => {
-  
-  $('.total-pfp').hover(() => {
-    $('.auth').css('width', '10em');
-    $('.auth').css('padding', '15px');
-    $('.auth').css('transform', 'skew(20deg)');
-    
-    $('.auth').css('transition-delay', '0s, 0s, 0s');
-    $('.total-pfp').css('width', 'calc(1em + 10px + 10em)');
-  }, () => {
-    $('.auth').css('width', '0em');
-    $('.auth').css('padding', '0px');
-    $('.auth').css('transform', 'skew(0deg)');
-    
-    $('.auth').css('transition-delay', '0s, 0s, 0.5s');
-    $('.total-pfp').css('width', 'calc(2em + 20px)');
-  });
-  
-  $('.stats > div:nth-of-type(1)').hover(() => {
-    $('.tool-tip').css('display', 'block');
-    $('.tool-tip').text('Coins');
-  }, () => {
-    $('.tool-tip').css('display', 'none');
-    $('.tool-tip').text('N/A');
-  });
-  
-  $('.stats > div:nth-of-type(2)').hover(() => {
-    $('.tool-tip').css('display', 'block');
-    $('.tool-tip').text('Ranked Wins');
-  }, () => {
-    $('.tool-tip').css('display', 'none');
-    $('.tool-tip').text('N/A');
-  });
-  
-  $('.stats > div:nth-of-type(3)').hover(() => {
-    $('.tool-tip').css('display', 'block');
-    $('.tool-tip').text('Total Wins');
-  }, () => {
-    $('.tool-tip').css('display', 'none');
-    $('.tool-tip').text('N/A');
-  });
-  
-  $('.stats > div:nth-of-type(4)').hover(() => {
-    $('.tool-tip').css('display', 'block');
-    $('.tool-tip').text('Rank');
-  }, () => {
-    $('.tool-tip').css('display', 'none');
-    $('.tool-tip').text('N/A');
-  });
-  
-  $('.stats > div:nth-of-type(5)').hover(() => {
-    $('.tool-tip').css('display', 'block');
-    $('.tool-tip').text('Streak');
-  }, () => {
-    $('.tool-tip').css('display', 'none');
-    $('.tool-tip').text('N/A');
-  });
-  
-  $('#room-btn').click(() => toggleRoomModal());
-  $('.room-modal-close').click(() => toggleRoomModal());
-  
-  $('#signup').click(() => toggleSignupModal());
-  $('.signup-modal-close').click(() => toggleSignupModal());
-  
-  $('#login').click(() => toggleLoginModal());
-  $('.login-modal-close').click(() => toggleLoginModal());
-  
-  $('.auth').click(e => e.preventDefault());
-  $('.total-pfp').click(e => e.preventDefault());
+  $(".total-pfp").hover(
+    () => {
+      $(".auth").css("width", "10em");
+      $(".auth").css("padding", "15px");
+      $(".auth").css("transform", "skew(20deg)");
 
+      $(".auth").css("transition-delay", "0s, 0s, 0s");
+      $(".total-pfp").css("width", "calc(1em + 10px + 10em)");
+    },
+    () => {
+      $(".auth").css("width", "0em");
+      $(".auth").css("padding", "0px");
+      $(".auth").css("transform", "skew(0deg)");
 
-  $('#signup-submit').click(() => {
-    let data = {
-        name: $('#signup-name').val(),
-        username: $('#signup-username').val(),
-        passcode: $('#signup-password').val()
+      $(".auth").css("transition-delay", "0s, 0s, 0.5s");
+      $(".total-pfp").css("width", "calc(2em + 20px)");
     }
-    socket.emit('signup-submit-req', data);
+  );
+
+  $(".stats > div:nth-of-type(1)").hover(
+    () => {
+      $(".tool-tip").css("display", "block");
+      $(".tool-tip").text("Coins");
+    },
+    () => {
+      $(".tool-tip").css("display", "none");
+      $(".tool-tip").text("N/A");
+    }
+  );
+
+  $(".stats > div:nth-of-type(2)").hover(
+    () => {
+      $(".tool-tip").css("display", "block");
+      $(".tool-tip").text("Ranked Wins");
+    },
+    () => {
+      $(".tool-tip").css("display", "none");
+      $(".tool-tip").text("N/A");
+    }
+  );
+
+  $(".stats > div:nth-of-type(3)").hover(
+    () => {
+      $(".tool-tip").css("display", "block");
+      $(".tool-tip").text("Total Wins");
+    },
+    () => {
+      $(".tool-tip").css("display", "none");
+      $(".tool-tip").text("N/A");
+    }
+  );
+
+  $(".stats > div:nth-of-type(4)").hover(
+    () => {
+      $(".tool-tip").css("display", "block");
+      $(".tool-tip").text("Rank");
+    },
+    () => {
+      $(".tool-tip").css("display", "none");
+      $(".tool-tip").text("N/A");
+    }
+  );
+
+  $(".stats > div:nth-of-type(5)").hover(
+    () => {
+      $(".tool-tip").css("display", "block");
+      $(".tool-tip").text("Streak");
+    },
+    () => {
+      $(".tool-tip").css("display", "none");
+      $(".tool-tip").text("N/A");
+    }
+  );
+
+  $("#room-btn").click(() => toggleRoomModal());
+  $(".room-modal-close").click(() => toggleRoomModal());
+
+  $("#learn-btn").click(() => toggleLearnModal());
+  $(".learn-modal-close").click(() => toggleLearnModal());
+
+  $("#store-btn").click(() => toggleStoreModal());
+  $(".store-modal-close").click(() => toggleStoreModal());
+
+  $("#signup").click(() => toggleSignupModal());
+  $(".signup-modal-close").click(() => toggleSignupModal());
+
+  $("#login").click(() => toggleLoginModal());
+  $(".login-modal-close").click(() => toggleLoginModal());
+
+  $(".auth").click((e) => e.preventDefault());
+  $(".total-pfp").click((e) => e.preventDefault());
+
+  $("#signup-submit").click(() => {
+    let data = {
+      name: $("#signup-name").val(),
+      username: $("#signup-username").val(),
+      passcode: $("#signup-password").val()
+    };
+    socket.emit("signup-submit-req", data);
   });
 
-  $('#login-submit').click(() => {
+  $("#login-submit").click(() => {
     console.log(1);
     let data = {
-        username: $('#login-username').val(),
-        passcode: $('#login-password').val()
-    }
-    socket.emit('login-submit-req', data);
+      username: $("#login-username").val(),
+      passcode: $("#login-password").val()
+    };
+    socket.emit("login-submit-req", data);
   });
 });
 
@@ -112,70 +132,90 @@ socket.on('signup-submit-res', data => {
     }
 });
 
-$(document).mousemove(function(event){
+$(document).mousemove(function (event) {
   $(".tool-tip").css("top", `${event.pageY}px`);
   $(".tool-tip").css("left", `${event.pageX}px`);
 });
 
 function toggleRoomModal() {
-  if ($('.room-modal').css('top') == '50px') {
-    $('.room-modal').css('top', '-100vh');
-    $('.blur-div').fadeOut();
+  if ($(".room-modal").css("top") == "50px") {
+    $(".room-modal").css("top", "-100vh");
+    $(".blur-div").fadeOut();
   } else {
-    $('.room-modal').css('top', '50px');
-    $('.blur-div').fadeIn();
+    $(".room-modal").css("top", "50px");
+    $(".blur-div").fadeIn();
+  }
+}
+
+function toggleLearnModal() {
+  if ($(".learn-modal").css("top") == "50px") {
+    $(".learn-modal").css("top", "-100vh");
+    $(".blur-div").fadeOut();
+  } else {
+    $(".learn-modal").css("top", "50px");
+    $(".blur-div").fadeIn();
+  }
+}
+
+function toggleStoreModal() {
+  if ($(".store-modal").css("top") == "50px") {
+    $(".store-modal").css("top", "-100vh");
+    $(".blur-div").fadeOut();
+  } else {
+    $(".store-modal").css("top", "50px");
+    $(".blur-div").fadeIn();
   }
 }
 
 function toggleSignupModal() {
-  if ($('.signup-modal').css('top') == '50px') {
-    $('.signup-modal').css('top', '-100vh');
-    $('.blur-div').fadeOut();
+  if ($(".signup-modal").css("top") == "50px") {
+    $(".signup-modal").css("top", "-100vh");
+    $(".blur-div").fadeOut();
   } else {
-    $('.signup-modal').css('top', '50px');
-    $('.blur-div').fadeIn();
+    $(".signup-modal").css("top", "50px");
+    $(".blur-div").fadeIn();
   }
 }
 
 function toggleLoginModal() {
-  if ($('.login-modal').css('top') == '50px') {
-    $('.login-modal').css('top', '-100vh');
-    $('.blur-div').fadeOut();
+  if ($(".login-modal").css("top") == "50px") {
+    $(".login-modal").css("top", "-100vh");
+    $(".blur-div").fadeOut();
   } else {
-    $('.login-modal').css('top', '50px');
-    $('.blur-div').fadeIn();
+    $(".login-modal").css("top", "50px");
+    $(".blur-div").fadeIn();
   }
 }
 
 function loadData() {
   if (userData.name) {
-    $('.welcome').show();
-    $('.signup').hide();
-    $('.welcome').text(`Hi, ${userData.name}`);
+    $(".welcome").show();
+    $(".signup").hide();
+    $(".welcome").text(`Hi, ${userData.name}`);
   } else {
-    $('.signup').show();
-    $('.welcome').hide();
+    $(".signup").show();
+    $(".welcome").hide();
   }
 
-
-  $('#coins-data').text(`${(userData.coins) ? userData.coins : ''}`);
-  $('#ranked-wins-data').text(`${(userData.rankedWins) ? userData.rankedWins : ''}`);
-  $('#total-wins-data').text(`${(userData.totalWins) ? userData.totalWins : ''}`);
-  $('#rank-data').text(`${(userData.rank) ? userData.rank : ''}`);
-  $('#streak-data').text(`${(userData.streak) ? userData.streak : ''}`);
+  $("#coins-data").text(`${userData.coins ? userData.coins : ""}`);
+  $("#ranked-wins-data").text(
+    `${userData.rankedWins ? userData.rankedWins : ""}`
+  );
+  $("#total-wins-data").text(`${userData.totalWins ? userData.totalWins : ""}`);
+  $("#rank-data").text(`${userData.rank ? userData.rank : ""}`);
+  $("#streak-data").text(`${userData.streak ? userData.streak : ""}`);
 }
 
 loadData();
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll("input");
 
-inputs.forEach(el => {
-  el.addEventListener('blur', e => {
-    if(e.target.value) {
-      e.target.classList.add('dirty');
+inputs.forEach((el) => {
+  el.addEventListener("blur", (e) => {
+    if (e.target.value) {
+      e.target.classList.add("dirty");
     } else {
-      e.target.classList.remove('dirty');
+      e.target.classList.remove("dirty");
     }
-  })
+  });
 });
-
